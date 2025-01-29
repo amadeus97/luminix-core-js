93d8d27358d24edcde87e805505963e400d68ba5
GabrielARamosDev <gabrihot97@hotmail.com>
2025-01-29 12:17:18 -0300

    fix: jest and babel configs

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,22 @@
 
 export default {
+    moduleFileExtensions: [ 'ts', 'js' ],
+    testEnvironment: 'jsdom',
+    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
     transform: {
-        // '^.+\\.ts?$': 'ts-jest',
-        // use babel for js
         '^.+\\.js?$': 'babel-jest',
         '^.+\\.ts?$': 'ts-jest',
     },
-    testEnvironment: 'jsdom',
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'js'],
-    
+    transformIgnorePatterns: [
+        'node_modules/(?!nanoevents|lodash-es)',
+    ],
+    // moduleDirectories: [
+    //     'node_modules'
+    // ],
+    // modulePaths: [
+    //     '<rootDir>'
+    // ],
+    // moduleNameMapper: {
+    //     '@luminix/support': require.resolve('@luminix/support'),
+    // },
 };
-
-
